a70e5f12d8b510d0cb5e16b2599d8897ac9dae21
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-08-13 09:51:12 +0300

    Expose PivotPoint core API

---
+++ pivotpoint/__init__.py
@@ -0,0 +1,21 @@
+"""PivotPoint: small actionable-present primitives."""
+
+from .core import (
+    ActionOffer,
+    Edge,
+    ModulatorState,
+    PivotDecision,
+    PivotPolicy,
+    PivotRuntime,
+    Signal,
+)
+
+__all__ = [
+    "ActionOffer",
+    "Edge",
+    "ModulatorState",
+    "PivotDecision",
+    "PivotPolicy",
+    "PivotRuntime",
+    "Signal",
+]
